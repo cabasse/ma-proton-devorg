dc856cf69650a32073e3ec6af864e3bd7484ae79
pi@serverpi (Mark Adcock) <markadcock@protonmail.com>
2020-08-23 23:45:48 -0400

    reset to 508beca, removed video... removed unused funcs

--- force-app/main/default/lwc/sfsCreditorTable/sfsCreditorTable.js
+++ force-app/main/default/lwc/sfsCreditorTable/sfsCreditorTable.js
@@ -140,27 +140,4 @@ export default class SfsCreditorTable extends LightningElement {
         return original;
     }
 
-
-    deleteRow(row) {
-        const { id } = row;
-        const index = this.findRowIndexById(id);
-        if (index !== -1) {
-            this.data = this.data
-                .slice(0, index)
-                .concat(this.data.slice(index + 1));
-        }
-    }
-
-    findRowIndexById(id) {
-        let ret = -1;
-        this.data.some((row, index) => {
-            if (row.id === id) {
-                ret = index;
-                return true;
-            }
-            return false;
-        });
-        return ret;
-    }
-
 }
